edc8765a459d25c1162c9ac32589e50f19fe377f
mike.wang <mike.wang@nextfortune.tw>
2025-02-18 09:03:35 +0800

    push test

--- TDD/TDD/obj/Debug/net8.0/TDD.AssemblyInfo.cs
+++ TDD/TDD/obj/Debug/net8.0/TDD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TDD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c94656adff0c7614e44e63518849e00b57588f02")]
 [assembly: System.Reflection.AssemblyProductAttribute("TDD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TDD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
